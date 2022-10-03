6195964fbd257ffe47b6c9f6a4c5a15835ad55cb
slightin <silentmiss@outlook.com>
2022-10-03 10:54:17 +0800

    updata doc

--- docs/.vuepress/.temp/internal/siteData.js
+++
@@ -1,1 +0,0 @@
-export const siteData = JSON.parse("{\"base\":\"/\",\"lang\":\"zh-CN\",\"title\":\"万事可乐\",\"description\":\"万事可乐的博客\",\"head\":[],\"locales\":{}}")
--- docs/.vuepress/.temp/md-enhance/reveal.js
+++
@@ -1,4 +0,0 @@
-import { reveal, revealMarkdown, revealHighlight, revealMath, revealSearch, revealNotes, revealZoom } from "F:/vuepress/node_modules/.pnpm/vuepress-plugin-md-enhance@2.0.0-beta.97/node_modules/vuepress-plugin-md-enhance/lib/client/reveal";
-
-export const useReveal = () => [reveal(), revealMarkdown(), revealHighlight(), revealMath(), revealSearch(), revealNotes(), revealZoom()
-];
